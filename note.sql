2a69ae8820af749f0cf1ae11e28ded21072decf1
b1gmeeeee <43607192+b1gme@users.noreply.github.com>
2022-01-30 11:35:08 +0800

    1月30日

--- note.sql
+++ note.sql
@@ -21,4 +21,16 @@ select * from easy_drinks where  main not in ('soda','iced tea');
 #NULL关键字使用
 select * from easy_drinks where not main is null;
 select * from easy_drinks where main  is not null;
-
+#数据库无法根据数据的顺序能得到最新的数据
+#delete 的正确使用
+delete from clown_info where activities = 'dancing'; #记得加上where要不然数据都会被删除
+#1、delete无法删除单个值，必须是整条数据或者多行数据
+# delete from table_A 这行语句会删除整个表的数据，需要谨慎~
+删除操作，首先需要插入数据，然后删除数据
+#做删除操作，可以先where找到数据，然后删除
+#update 只是更新需要改变的某一列的值
+update doughhut_ratings set type = 'glazed' where type = 'plain glazed';
+update your_table set first_column = 'newvalue',second_column = 'another_value';
+#update 不仅可以修改列的值大小，还可以更新列名。
+#若将where语句省略，表中的每一行数据都改成set后面的语句了
+#怕更新错错误，可以巧妙的使用where语句
