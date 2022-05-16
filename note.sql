718f3c2259731788da6c1303f3479981790fa53e
b1gmeeeee <43607192+b1gme@users.noreply.github.com>
2022-05-16 23:27:54 +0800

    23.27

--- note.sql
+++ note.sql
@@ -130,7 +130,7 @@ select prod_name, prod_id from products where prod_name like '_ ton anvil';
 select prod_name from products where prod_name regexp '[123] ton' order by prod_name;
 select prod_name from products where prod_name regexp '[1|2|3] ton' order by prod_name;
 #正则表达式
-符号“[]”表示或 ^表示否定 [^123]匹配除这些字符外的任何东西 []符号是另一种形式的or语句
+符号“[]”表示用来匹配特定的字符 ^表示否定 [^123]匹配除这些字符外的任何东西 []符号是另一种形式的or语句
 #正则表达式
 [0123456789] 等价于[0-9]集合将匹配数字0到9 [a-z]可以匹配任意字母字符
 为了匹配特殊字符，必须使用\\为前导。\\-表示查找-，\\.表示查找.#\\表示转义符号 正则表达式内具有特殊意义的所有字符都
@@ -151,3 +151,14 @@ select prod_name from products where prod_name regexp '[0-9][0-9][0-9][0-9]' ord
 上述方法等价
 [:digit:]匹配任意数字，因而他为数字的一个集合。{4}确切地要求它前面的字符（任意数字）出现4次，所以[[:digit:]]{4}匹配连在一起的任意
 4位数字
+元 字 符 说 明
+^ 文本的开始
+$ 文本的结尾
+[[:<:]] 词的开始
+[[:>:]] 词的结尾
+select prod_name from products where prod_name regexp '^[0-9\\.]' order by prod_name;
+^匹配串的开始，即^[0-9\\.]只在.或任意数字为串中第一个字符时才匹配它们
+#^的双重用途：1、在集合中（用[和]定义），用它来否定该集合；2、用来指串的开始处
+#使regexp起类似like的作用 like和regexp不同在于like匹配整个串而regexp匹配子串。利用定位符，通过^开始每个表达式，
+用$结束每个表达式，可以使regexp的作用与like一样
+#简单的正则表达式测试（不适用数据库表的情况）select 'hello' regexp '[0-9]'
